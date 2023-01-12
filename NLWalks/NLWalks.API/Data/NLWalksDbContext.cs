bb1d256ef41618b60e290655c0e7bc49464e7d84
devika <devikamurali08@gmail.com>
2023-01-12 07:42:27 +0530

    end of section2

---
+++ NLWalks/NLWalks.API/Data/NLWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLWalks.API.Models.Domain;
+
+namespace NLWalks.API.Data
+{
+    public class NLWalksDbContext:DbContext
+    {
+        public NLWalksDbContext(DbContextOptions<NLWalksDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
